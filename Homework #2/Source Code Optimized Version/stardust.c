c85fde9c7b898aff55c504d4c90988c9c976364e
Chelcea Claudiu <claudiuchelcea01@gmail.com>
2021-02-21 16:26:44 +0200

    Update stardust.c

--- Homework #2/Source Code Optimized Version/stardust.c
+++ Homework #2/Source Code Optimized Version/stardust.c
@@ -1,16 +1,16 @@
-#include <stdio.h>
-#include <stdlib.h>
 #include "Input-Output.h"
 #include "Modify_matrix.h"
+#include <stdio.h>
+#include <stdlib.h>
 
-int main(void) {
-
+int main(void)
+{
 	// The numbers of lines in the matrix
 	int nr_lines = 0;
-	scanf("%d",&nr_lines);
+	scanf("%d", &nr_lines);
 
 	// The numbers of elements on every line
-	int* line_elements_nr = NULL;
+	int *line_elements_nr = NULL;
 	alloc_line_elements(&line_elements_nr, nr_lines);
 
 	// The matrix that holds the scanned numbers
@@ -19,23 +19,24 @@ int main(void) {
 
 	// Task 1
 	// Display the arithmetic mean calculated with the bytes on the margins
-	show_arithmetic_mean_of_byte_margins(line_elements_nr,nr_lines,pixel_matrix);
-	
+	show_arithmetic_mean_of_byte_margins(line_elements_nr, nr_lines,
+										 pixel_matrix);
+
 	// Task 2
 	// Execute the modifications
 	printf("task 2\n");
 	execute_modifications(&pixel_matrix, &line_elements_nr);
 
 	// Display the matrix in HEX format
-	print_matrix(pixel_matrix,nr_lines,line_elements_nr);
+	print_matrix(pixel_matrix, nr_lines, line_elements_nr);
 
 	// Task 3
+	// Find the biggest black hole
 	printf("task 3\n");
+	display_biggest_black_hole(pixel_matrix, line_elements_nr, nr_lines);
 
 	// Release the memory
-	free_mem(&line_elements_nr,nr_lines,&pixel_matrix);
-
-
+	free_mem(&line_elements_nr, nr_lines, &pixel_matrix);
 
 	return SUCCESS_FINISH;
 }
